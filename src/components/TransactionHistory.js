175a454f4c5cdfcefa5cf2e7ea594f078ea2e3bf
Rishi_696 <rishi102001@gmail.com>
2023-08-28 08:30:07 +0530

    modified transaction history.js

--- src/components/TransactionHistory.js
+++ src/components/TransactionHistory.js
@@ -1,18 +1,165 @@
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import './TransactionHistory.css';
+
+// const TransactionHistory = () => {
+//   const [selectedAccountId, setSelectedAccountId] = useState('');
+//   const [accountOptions, setAccountOptions] = useState([]);
+//   const [transactions, setTransactions] = useState({
+//     debits: [],
+//     credits: [],
+//     withdrawals: [],
+//   });
+
+//   useEffect(() => {
+//     fetchAccountOptions();
+//   }, []);
+
+//   useEffect(() => {
+//     if (selectedAccountId !== '') {
+//       fetchTransactions();
+//     }
+//   }, [selectedAccountId]);
+
+//   const fetchAccountOptions = async () => {
+//     try {
+//       var userId = window.sessionStorage.getItem('userId');
+//       const response = await axios.get(
+//         'http://localhost:8080/account/readCustomer/' + userId
+//       );
+//       console.log(response.data);
+//       var accounts = response.data;
+//       var accountids = accounts.map((account) => account.account_id);
+//       setAccountOptions(accountids);
+//     } catch (error) {
+//       console.error('Error fetching account options:', error);
+//     }
+//   };
+
+//   const fetchTransactions = async () => {
+//     try {
+//       const response = await axios.get(
+//         `http://localhost:8080/customer/transaction/` + selectedAccountId
+//       );
+//       console.log('transactions');
+//       console.log(response.data);
+//       setTransactions(response.data); // Assuming the response contains transaction data
+//     } catch (error) {
+//       console.error('Error fetching transactions:', error);
+//     }
+//   };
+
+//   const debits = transactions.debits || [];
+//   const credits = transactions.credits || [];
+//   const withdrawals = transactions.withdrawals || [];
+
+//   return (
+//     <div className="transaction-history">
+//       <h1>Transaction History</h1>
+//       <div className="account-dropdown">
+//         <label>Select Account:</label>
+//         <select
+//           value={selectedAccountId}
+//           onChange={(e) => setSelectedAccountId(e.target.value)}
+//         >
+//           <option value="">Select an account</option>
+//           {accountOptions.map((accountId) => (
+//             <option key={accountId} value={accountId}>
+//               {accountId}
+//             </option>
+//           ))}
+//         </select>
+//       </div>
+//       <div className="transaction-tables">
+//         <div className="transaction-table">
+//           <h2>Debits</h2>
+//           <table>
+//             <thead>
+//               <tr>
+//                 <th>Transaction Time</th>
+//                 <th>From Account</th>
+//                 <th>To Account</th>
+//                 <th>Transaction Type</th>
+//                 <th>Amount</th>
+//               </tr>
+//             </thead>
+//             <tbody>
+//               {debits.map((transaction) => (
+//                 <tr key={transaction.transaction_id}>
+//                   <td>{transaction.transaction_time}</td>
+//                   <td>{transaction.from_account}</td>
+//                   <td>{transaction.to_account}</td>
+//                   <td>{transaction.tType}</td>
+//                   <td>{transaction.amount}</td>
+//                 </tr>
+//               ))}
+//             </tbody>
+//           </table>
+//         </div>
+//         <div className="transaction-table">
+//           <h2>Credits</h2>
+//           <table>
+//             <thead>
+//               <tr>
+//                 <th>Transaction Time</th>
+//                 <th>From Account</th>
+//                 <th>To Account</th>
+//                 <th>Transaction Type</th>
+//                 <th>Amount</th>
+//               </tr>
+//             </thead>
+//             <tbody>
+//               {credits.map((transaction) => (
+//                 <tr key={transaction.transaction_id}>
+//                   <td>{transaction.transaction_time}</td>
+//                   <td>{transaction.from_account}</td>
+//                   <td>{transaction.to_account}</td>
+//                   <td>{transaction.tType}</td>
+//                   <td>{transaction.amount}</td>
+//                 </tr>
+//               ))}
+//             </tbody>
+//           </table>
+//         </div>
+//         <div className="transaction-table">
+//           <h2>Withdrawals</h2>
+//           <table>
+//             <thead>
+//               <tr>
+//                 <th>Transaction Time</th>
+//                 <th>From Account</th>
+//                 <th>To Account</th>
+//                 <th>Transaction Type</th>
+//                 <th>Amount</th>
+//               </tr>
+//             </thead>
+//             <tbody>
+//               {withdrawals.map((transaction) => (
+//                 <tr key={transaction.transaction_id}>
+//                   <td>{transaction.transaction_time}</td>
+//                   <td>{transaction.from_account}</td>
+//                   <td>{transaction.to_account}</td>
+//                   <td>{transaction.tType}</td>
+//                   <td>{transaction.amount}</td>
+//                 </tr>
+//               ))}
+//             </tbody>
+//           </table>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default TransactionHistory;
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './TransactionHistory.css';
 
-var selected_acc;
-
 const TransactionHistory = () => {
   const [selectedAccountId, setSelectedAccountId] = useState('');
   const [accountOptions, setAccountOptions] = useState([]);
-  const [transactions, setTransactions] = useState({
-    debits: [],
-    credits: [],
-    withdrawals: [],
-  });
-
+  const [transactions, setTransactions] = useState([]);
 
   useEffect(() => {
     fetchAccountOptions();
@@ -20,7 +167,7 @@ const TransactionHistory = () => {
 
   useEffect(() => {
     if (selectedAccountId !== '') {
-      fetchTransactions();
+      fetchTransactions(selectedAccountId);
     }
   }, [selectedAccountId]);
 
@@ -30,37 +177,25 @@ const TransactionHistory = () => {
       const response = await axios.get(
         'http://localhost:8080/account/readCustomer/' + userId
       );
-      console.log(response.data);
-      var accounts = response.data;
-      var accountids = accounts.map((account) => account.account_id);
+      const accounts = response.data;
+      const accountids = accounts.map((account) => account.account_id);
       setAccountOptions(accountids);
     } catch (error) {
       console.error('Error fetching account options:', error);
     }
   };
 
-  const fetchTransactions = async () => {
+  const fetchTransactions = async (accountId) => {
     try {
       const response = await axios.get(
-        `http://localhost:8080/customer/transaction/` + selected_acc
+        `http://localhost:8080/customer/transaction/` + accountId
       );
-      console.log("transactions")
-      console.log(response.data);
-      setTransactions(response.data); // Assuming the response contains transaction data
+      console.log('transactions', response.data);
+      setTransactions(response.data);
     } catch (error) {
       console.error('Error fetching transactions:', error);
     }
   };
-
-  const handleAccountSelect = (accountId) => {
-    selected_acc = accountId;
-    setSelectedAccountId(accountId);
-  };
-
-  const debits = transactions.debits;
-const credits = transactions.credits;
-const withdrawals = transactions.withdrawals;
-
 
   return (
     <div className="transaction-history">
@@ -69,7 +204,7 @@ const withdrawals = transactions.withdrawals;
         <label>Select Account:</label>
         <select
           value={selectedAccountId}
-          onChange={(e) => handleAccountSelect(e.target.value)}
+          onChange={(e) => setSelectedAccountId(e.target.value)}
         >
           <option value="">Select an account</option>
           {accountOptions.map((accountId) => (
@@ -81,52 +216,26 @@ const withdrawals = transactions.withdrawals;
       </div>
       <div className="transaction-tables">
         <div className="transaction-table">
-          <h2>Debits</h2>
+          <h2>All Transactions</h2>
           <table>
-            {/* Table header */}
+            <thead>
+              <tr>
+                <th>Transaction Time</th>
+                <th>From Account</th>
+                <th>To Account</th>
+                <th>Transaction Type</th>
+                <th>Amount</th>
+              </tr>
+            </thead>
             <tbody>
-              {debits.map((transaction) => (
+              {transactions.map((transaction) => (
                 <tr key={transaction.transaction_id}>
                   <td>{transaction.transaction_time}</td>
-          <td>{transaction.from_account}</td>
-          <td>{transaction.to_account}</td>
-          <td>{transaction.tType}</td>
-          <td>{transaction.amount}</td>
+                  <td>{transaction.from_account}</td>
+                  <td>{transaction.to_account}</td>
+                  <td>{transaction.tType}</td>
+                  <td>{transaction.amount}</td>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-        <div className="transaction-table">
-          <h2>Credits</h2>
-          <table>
-            {/* Table header */}
-            <tbody>
-              {credits.map((transaction) => (
-                <tr key={transaction.transaction_id}>
-                <td>{transaction.transaction_time}</td>
-        <td>{transaction.from_account}</td>
-        <td>{transaction.to_account}</td>
-        <td>{transaction.tType}</td>
-        <td>{transaction.amount}</td>
-              </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-        <div className="transaction-table">
-          <h2>Withdrawals</h2>
-          <table>
-            {/* Table header */}
-            <tbody>
-              {withdrawals.map((transaction) => (
-               <tr key={transaction.transaction_id}>
-               <td>{transaction.transaction_time}</td>
-       <td>{transaction.from_account}</td>
-       <td>{transaction.to_account}</td>
-       <td>{transaction.tType}</td>
-       <td>{transaction.amount}</td>
-             </tr>
               ))}
             </tbody>
           </table>
